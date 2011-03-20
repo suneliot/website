7e1d4e086cab523c7b5956d2f0a341ec18fa7d49
Eliot Sun <suneliot@gmail.com>
2011-03-19 20:58:31 -0700

    added google analytics

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,3 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 end
 
-class ApplicationController
-  before_filter :ensure_domain
-
-  APP_DOMAIN = 'eliotsun.heroku.com'
-
-  def ensure_domain
-    if request.env['HTTP_HOST'] != APP_DOMAIN
-      # HTTP 301 is a "permanent" redirect
-      redirect_to "http://www.eliotsun.com", :status => 301
-    end
-  end
-end
